758ee9272bb5a2ece847a3bcd2d403fc62349f8b
Brett Kuhn <brettkuhn87@gmail.com>
2022-08-30 09:08:00 -0600

    Built dragon class w/ great and eat functions

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,20 @@
+class Dragon {
+  constructor(name, riderName){
+    this.name = name
+    this.rider = riderName
+    this.timesEaten = 0
+    this.hungry = true
+  }
+  greet(){
+    return `Hi, ${this.rider}!`
+  }
+  eat(){
+    this.timesEaten++
+    if (this.timesEaten >= 3){
+      this.hungry = false
+    }
+  }
+}
 
+
+module.exports = Dragon
