075f7ea87507286661db191f98d9209087fad8e8
Brett Kuhn <brettkuhn87@gmail.com>
2022-08-29 18:27:22 -0600

    Passed unicorn tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,22 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
+    if (color === undefined){
+      this.color = "white";
+    } else {
     this.color = color;
+    }
+  }
+
+  isWhite(color){
+    if (this.color === "white"){
+      return true
+    } else {
+      return false
+    }
+  }
+  says(speechAct){
+    return `**;* ${speechAct} *;**`
   }
 }
 
