9eb0e61004fa35b29da97e9d77f435614dadb41a
Brett Kuhn <brettkuhn87@gmail.com>
2022-09-02 10:42:37 -0600

    Passed magician tests except for last two not working

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,36 @@
+class Magician {
+  constructor(magician){
+    this.name = `The Great ${magician.name}`
+    this.assistant = magician.assistant
+    if (magician.clothing === undefined){
+    this.favoriteAccessory = "top hat"
+    } else {
+    this.favoriteAccessory = magician.clothing
+    }
+    this.confidencePercentage = 10
+  }
+
+  performIncantation(incantation){
+    return `${incantation.toUpperCase()}!`
+  }
+
+  performTrick(){
+    if (this.favoriteAccessory === "top hat"){
+      this.confidencePercentage += 10
+      return "PULL RABBIT FROM TOP HAT"
+    } else {
+      this.confidencePercentage += 10
+      return "PULL DOVE FROM SLEEVE"
+    }
+  }
+
+  performShowStopper(){
+    if (this.confidencePercentage < 100 && this.assistant === false){
+      return "Oh no, this trick is not ready!"
+    } else {
+      return "WOW! The magician totally just sawed that person in half!"
+    }
+  }
+}
+
+module.exports = Magician;
