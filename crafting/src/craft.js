c47655948a96898b3f7ad1646ef061f7ca98914a
Brett Kuhn <brettkuhn87@gmail.com>
2022-09-19 16:21:33 -0600

    Finish crafting test suite

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,23 @@
 class Craft {
+  constructor(craft) {
+    this.name = craft.type;
+    this.materials = craft.materials;
+    this.completed = false;
+  }
+  completeCraft() {
+    this.completed = true;
+    return `All done! It looks great!`
+  }
+  calculateProjectTotal() {
+    var projectTotal = 0;
+    
+    for (var i = 0; i < this.materials.length; i++) {
+      projectTotal += this.materials[i].calculateMaterialCost()
+    }
 
+    return projectTotal
+    // return this.materials[0].calculateMaterialCost() + this.materials[1].calculateMaterialCost()
+  }
 }
 
 module.exports = Craft;
--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,21 @@
 class Material {
-
+  constructor(type, price, amount, units) {
+    this.name = type;
+    this.price = price;
+    this.amount = amount;
+    this.units = units;
+  }
+  useMaterial(number) {
+    if (this.amount - number >= 0 ) {
+      this.amount = this.amount - number;
+      return `You now have ${this.amount} ${this.units} of ${this.name} left.`
+    } else {
+      return `You don't have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`
+    }
+  }
+  calculateMaterialCost() {
+    return this.price * this.amount;
+  }
 }
 
 module.exports = Material;
