2c8c3ca32e83d84eba3a9dab921be87fbc62e03d
Brett Kuhn <brettkuhn87@gmail.com>
2022-09-20 12:00:06 -0600

    Finish road race test suite

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,21 @@
 class Roadrace {
-
+  constructor(race) {
+    this.name = race.title;
+    this.location = race.city;
+    this.participants = [];
+  }
+  setDistance(length) {
+    this.distance = length;
+    return `The ${this.name} in ${this.location} is a ${length} mile race.`
+  }
+  registerParticipants(runner) {
+    this.participants.push(runner);
+  }
+  completeRace() {
+    for (var i = 0; i < this.participants.length; i++) {
+      this.participants[i].runRace(this.name, this.distance);
+    }
+  }
 }
 
 module.exports = Roadrace;
